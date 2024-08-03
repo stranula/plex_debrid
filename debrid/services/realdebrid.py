dbd92d10e3890163c7b4d5ba07c025eba15ccf83
stranula <stranula@gmail.com>
2024-08-02 22:31:08 -0400

    Update realdebrid.py

--- debrid/services/realdebrid.py
+++ debrid/services/realdebrid.py
@@ -179,8 +179,6 @@ def download(element, stream=True, query='', force=False):
     # Extract data for CSV
     data = extract_element_data(element)
     cached = element.Releases
-    print("Cached: ")
-    print(cached)
     if query == '':
         query = element.deviation()
     wanted = [query]
